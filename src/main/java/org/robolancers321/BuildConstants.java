ba9f9060cd94c0957e1db5d948040965a195c59a
Raphael Hoang <fluffier913@gmail.com>
2024-01-26 17:13:02 -0500

    Added FF to intake

--- src/main/java/org/robolancers321/BuildConstants.java
+++ src/main/java/org/robolancers321/BuildConstants.java
@@ -1,19 +1,18 @@
+/* (C) Robolancers 2024 */
 package org.robolancers321;
 
-/**
- * Automatically generated file containing build version information.
- */
+/** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "321-Crescendo-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 6;
-  public static final String GIT_SHA = "6377635d69d5713f8f8b5400fc7a136abe5c431e";
-  public static final String GIT_DATE = "2024-01-21 18:36:14 EST";
-  public static final String GIT_BRANCH = "dev-intake";
-  public static final String BUILD_DATE = "2024-01-24 08:18:40 EST";
-  public static final long BUILD_UNIX_TIME = 1706102320667L;
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "b44f39d3a22624c84fd1b456528f344fd33d9129";
+  public static final String GIT_DATE = "2024-01-24 17:48:41 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-26 15:35:24 EST";
+  public static final long BUILD_UNIX_TIME = 1706301324002L;
   public static final int DIRTY = 0;
 
-  private BuildConstants(){}
+  private BuildConstants() {}
 }
